7c72f7a65aaaa47ab7dedce9802ef6fce8087e5c
huai <61614490+harui2019@users.noreply.github.com>
2021-03-07 17:50:58 +0800

    upload page install @ 20210307 1750

---
+++ Herd-master/load.js
@@ -0,0 +1,11 @@
+
+anime({
+    targets: '.flash',
+    opacity: [0.01,1],
+    loop: true,
+    duration: 1800,
+    easing: 'easeInQuad'
+});
+
+
+setTimeout("location.href='./result.html' ",3000);
